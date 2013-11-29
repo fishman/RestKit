e70df65f1c391e421184f22a852ad402dd774d6f
Samuel E. Giddins <segiddins@segiddins.me>
2013-11-29 13:19:16 -0500

    Manually call _gcov_flush()

--- Tests/RKTestEnvironment.m
+++ Tests/RKTestEnvironment.m
@@ -69,3 +69,51 @@
 }
 
 @end
+
+#ifdef GCC_GENERATE_TEST_COVERAGE_FILES
+
+@interface RKTestCoverageLog : SenTestLog
+@end
+
+// GCOV Flush function
+extern void __gcov_flush(void);
+
+static id mainSuite = nil;
+
+@implementation RKTestCoverageLog
+
++ (void)initialize
+{
+	[[NSUserDefaults standardUserDefaults] setValue:@"RKTestCoverageLog" forKey:SenTestObserverClassKey];
+    
+	[super initialize];
+}
+
++ (void)testSuiteDidStart:(NSNotification *)notification
+{
+	[super testSuiteDidStart:notification];
+    
+	SenTestSuiteRun *suite = notification.object;
+    
+	if (mainSuite == nil)
+	{
+		mainSuite = suite;
+	}
+}
+
++ (void)testSuiteDidStop:(NSNotification *)notification
+{
+	[super testSuiteDidStop:notification];
+    
+	SenTestSuiteRun* suite = notification.object;
+    
+	if (mainSuite == suite)
+	{
+		// workaround for missing flush with iOS 7 Simulator
+		__gcov_flush();
+	}
+}
+
+@end
+
+#endif
